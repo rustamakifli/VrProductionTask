a4127b2f059ea3040fad6654346ec11cd485d6e7
Rustam Akifli <rustamakifli2003@icloud.com>
2024-06-26 18:20:00 +0400

    default page updated

--- vrproductiontask/vrproductiontask/urls.py
+++ vrproductiontask/vrproductiontask/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('users.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('',lambda request: redirect('blogs'),permanent=True),
 
 ]
 
